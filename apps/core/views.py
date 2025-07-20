b2673fbdc2a0a3afc37a0a52ad21279b6c8a70d5
laghazar <larissaghazaryan@gmail.com>
2025-07-20 09:12:25 +0400

    09:12

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
-
 from apps.reporting.views import report
 from .models import SiteStatistics, Character
 from apps.reporting.models import PhishingReport
@@ -9,32 +8,30 @@ from apps.quiz.models import QuizResult, QuizAttempt
 from apps.url_checker.models import URLCheck
 from .utils import update_statistics
 
-from django.shortcuts import render 
 def home(request):
     """Գլխավոր էջ"""
     stats, created = SiteStatistics.objects.get_or_create(pk=1)
     ara_character = Character.objects.filter(character_type='ara').first()
     shamiram_character = Character.objects.filter(character_type='shamiram').first()
     
+    # Թարմացնել վիճակագրությունը update_statistics-ից
+    updated_stats = update_statistics()
+    
     context = {
         'stats': stats,
         'ara_character': ara_character,
         'shamiram_character': shamiram_character,
+        'updated_stats': updated_stats,  # Ավելացնել update_statistics-ի տվյալները
         'page_title': 'CyberAratta - Կիբեռանվտանգության Կրթական Հարթակ'
     }
     
-    return render(request, 'core/home.html',)
-
-def home(request):
-    stats = update_statistics()
-    return render(request, 'core/home.html', {'stats': stats})
+    return render(request, 'core/home.html', context)
 
 def update_statistics():
     stats = {
         'checked_urls': URLCheck.objects.count(),
-        'detected_threats': URLCheck.objects.filter(is_malicious=True).count(),
+        'detected_threats': URLCheck.objects.filter(status='malicious').count(),  # Փոխարինել is_malicious-ը status-ով
         'completed_quizzes': QuizAttempt.objects.count(),
         'reports': report.objects.count(),
     }
     return stats
-
